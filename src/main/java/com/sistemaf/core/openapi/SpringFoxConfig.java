f6618f55a7e9ce9f9fb97962f15a1674f739ed17
Felipe Souza <felipedb91@gmail.com>
2022-09-03 13:44:24 -0300

    chore: add security config for swagger api

--- src/main/java/com/sistemaf/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/sistemaf/core/openapi/SpringFoxConfig.java
@@ -1,6 +1,11 @@
 package com.sistemaf.core.openapi;
 
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,9 +13,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows( password = @OAuthFlow(
+                tokenUrl = "oauth/token",
+                scopes = {
+                        @OAuthScope(name = "read", description = "read scope"),
+                        @OAuthScope(name = "write", description = "write scope")
+                }
+
+            )
+        )
+)
 public class SpringFoxConfig {
-
-
     @Bean
     public OpenAPI apiInfo() {
         return new OpenAPI()
