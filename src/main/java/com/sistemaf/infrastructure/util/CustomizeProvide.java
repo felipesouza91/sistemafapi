d32d647e08c9a0afa55d6cda4fdf38439181d5ef
Felipe Souza <felipedb91@gmail.com>
2022-09-05 22:57:49 -0300

    remove: remove database schedule.

--- src/main/java/com/sistemaf/infrastructure/util/CustomizeProvide.java
+++ src/main/java/com/sistemaf/infrastructure/util/CustomizeProvide.java
@@ -1,17 +1,13 @@
 package com.sistemaf.infrastructure.util;
 
-import java.util.Properties;
-
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.sistemaf.infrastructure.util.integration.ApiConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
-import com.sistemaf.infrastructure.util.integration.ApiConnection;
-import com.smattme.MysqlExportService;
 
 @Component
 public class CustomizeProvide {
@@ -34,29 +30,4 @@ public class CustomizeProvide {
 	    return new Hibernate5Module();
 	}
 
-	@Bean
-	public MysqlExportService mysqlExportService() {
-		return new MysqlExportService(this.getMysqlProperties());
-	}
-	
-	private Properties getMysqlProperties() {
-		Properties properties = new Properties();
-		properties.setProperty(MysqlExportService.DB_NAME, this.getDataBaseName());
-		properties.setProperty(MysqlExportService.DB_USERNAME, env.getProperty("spring.datasource.username"));
-		properties.setProperty(MysqlExportService.DB_PASSWORD, env.getProperty("spring.datasource.password"));
-		properties.setProperty(MysqlExportService.PRESERVE_GENERATED_ZIP, "true");
-		return properties;
-	}
-	
-	private String getDataBaseName() {
-		String url = env.getProperty("spring.datasource.url");
-
-		int indexOfbase = url.lastIndexOf("/");
-		int lastIndex = url.indexOf("?");
-		if(lastIndex == -1 ) {
-			 lastIndex = url.length();
-		}
-		String nome = url.substring(indexOfbase+1, lastIndex); 
-		return nome;
-	}
 }
