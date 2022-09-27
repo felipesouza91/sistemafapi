ae2eae5e8875fc07b7951da42b08db45b3a89f46
Felipe Souza <felipedb91@gmail.com>
2022-09-26 23:27:58 -0300

    feat: configure Docs to use Authorization Code

--- src/main/java/com/sistemaf/core/openapi/OpenApiConfig.java
+++ src/main/java/com/sistemaf/core/openapi/OpenApiConfig.java
@@ -9,22 +9,22 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2,
-        flows = @OAuthFlows( password = @OAuthFlow(
-                tokenUrl = "oauth/token",
-                scopes = {
-                        @OAuthScope(name = "read", description = "read scope"),
-                        @OAuthScope(name = "write", description = "write scope")
-                }
+        flows = @OAuthFlows( authorizationCode = @OAuthFlow(
+            authorizationUrl = "${springdoc.oAuthFlow.authorizationUrl}",
+                tokenUrl = "${springdoc.oAuthFlow.tokenUrl}",
+                scopes = {@OAuthScope(name = "READ"), @OAuthScope(name = "WRITE")}
 
-            )
+          )
+
         )
 )
-public class SpringFoxConfig {
+public class OpenApiConfig {
     @Bean
     public OpenAPI apiInfo() {
         return new OpenAPI()
@@ -36,7 +36,7 @@ public class SpringFoxConfig {
                                         .name("Felipe Santana")
                                         .url( "https://fsantana.dev" )))
 
-                ;
+                .addSecurityItem(new SecurityRequirement().addList("client_id", "angular"));
     }
 
 
