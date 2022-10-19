cd69686b324ceb8129c8496cf54dd44e5e2697bd
Felipe Souza <felipedb91@gmail.com>
2022-10-18 22:07:40 -0300

    test: refactor ClientInformationUnitTest

--- src/main/java/com/sistemaf/domain/service/ClienteService.java
+++ src/main/java/com/sistemaf/domain/service/ClienteService.java
@@ -1,23 +1,20 @@
 package com.sistemaf.domain.service;
 
-import java.util.Optional;
-
-import javax.validation.Valid;
-
 import com.sistemaf.domain.exception.BusinessException;
 import com.sistemaf.domain.exception.EntityNotFoundException;
 import com.sistemaf.domain.filter.ClienteFilter;
 import com.sistemaf.domain.model.Cliente;
 import com.sistemaf.domain.repository.bairro.BairroRepository;
 import com.sistemaf.domain.repository.cliente.ClienteRepository;
-
 import com.sistemaf.domain.repository.grupo.GrupoRepository;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 
 @Service
@@ -74,7 +71,7 @@ public class ClienteService {
 
 	private Cliente getClienteOptional(Long codigo) {
 		Optional<Cliente> clientOptional = clienteRepository.findById(codigo);
-		return clientOptional.orElseThrow(()-> new EntityNotFoundException("O Cliente encontrado ou não existe"));
+		return clientOptional.orElseThrow(()-> new EntityNotFoundException("O Cliente não encontrado ou não existe"));
 	}
 
 	private void verifyNeighborhood(Long  neighborhoodCode) {
--- src/test/java/com/sistemaf/domain/service/ClientInformationServiceUnitTest.java
+++ src/test/java/com/sistemaf/domain/service/ClientInformationServiceUnitTest.java
@@ -6,31 +6,27 @@ import com.sistemaf.domain.filter.InformacaoFilter;
 import com.sistemaf.domain.model.Cliente;
 import com.sistemaf.domain.model.ClienteInformacao;
 import com.sistemaf.domain.repository.cliente.ClienteInformacaoRepository;
-
 import com.sistemaf.util.FactoryModels;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-
-import static org.mockito.Mockito.*;
-import org.mockito.*;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import java.util.Arrays;
 import java.util.Optional;
 
-@RunWith(MockitoJUnitRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class ClientInformationServiceUnitTest {
 
     @Mock
@@ -44,54 +40,62 @@ public class ClientInformationServiceUnitTest {
     @InjectMocks
     private ClientInformationService sut;
 
-    @Before
-    public void setup() {
-        when(clientService.listarPorCodigo(any())).thenReturn(FactoryModels.getCliente());
-        when(informacaoDao.findById(any())).thenReturn(Optional.of(FactoryModels.getClientInfo()));
-        when(informacaoDao.filtrar(any(),any(), any()))
+    @BeforeEach
+    void setup() {
+        lenient().when(clientService.listarPorCodigo(any())).thenReturn(FactoryModels.getCliente());
+        lenient().when(informacaoDao.findById(any())).thenReturn(Optional.of(FactoryModels.getClientInfo()));
+        lenient().when(informacaoDao.filtrar(any(),any(), any()))
                 .thenReturn(new PageImpl<>(Arrays.asList(FactoryModels.getClientInfo(), FactoryModels.getClientInfo()), pageRequest, 1));
-        when(informacaoDao.save(any())).thenReturn(FactoryModels.getClientInfo());
+        lenient().when(informacaoDao.save(any())).thenReturn(FactoryModels.getClientInfo());
+    }
+
+    @Test()
+    public void giveValidClientId_whenFindClientInfoById_success() {
+        ClienteInformacao info = sut.getInformacaoById(1L);
+        assertNotNull(info);
+        assertEquals(info.getId(), 1L);
     }
 
     @Test
-    public void giveValidClientId_whenFindClientInfoById_success() {
-        ClienteInformacao info = sut.getInformacaoById(1L);
-        Assert.assertNotNull(info);
-        assertThat(info.getId(), is(1L));
-    }
-
-    @Test(expected = EntityNotFoundException.class)
     public void giveInvalidClientInfoId_whenFindById__fails() {
         when(informacaoDao.findById(any())).thenReturn(Optional.empty());
-        sut.getInformacaoById(1L);
+        Exception exception = assertThrows(EntityNotFoundException.class, () ->  sut.getInformacaoById(1L));
+        assertTrue(exception instanceof EntityNotFoundException);
+        assertEquals("Informação não foi encontrada", exception.getMessage());
     }
 
     @Test
     public void giveValidClientId_whenAllClientInfo_success() {
         InformacaoFilter filter = new InformacaoFilter();
         Page<ClienteInformacao> result = sut.filtrarAtendimento(1L, filter, pageRequest);
-        Assert.assertNotNull(result.getContent());
-        assertThat(result.getContent().get(0).getId(), is(FactoryModels.getClientInfo().getId()));
-        assertThat(result.getTotalElements(), is(1L));
+        assertNotNull(result.getContent());
+        assertEquals(result.getContent().get(0).getId(), FactoryModels.getClientInfo().getId());
+        assertEquals(result.getTotalElements(), 1L);
     }
 
-    @Test(expected = BusinessException.class)
+    @Test
     public void givenNullClientId_whenAllClientInfo_fails() {
         InformacaoFilter filter = new InformacaoFilter();
-        Page<ClienteInformacao> result = sut.filtrarAtendimento(null, filter, pageRequest);
+        Exception exception = assertThrows(BusinessException.class, () -> sut.filtrarAtendimento(null, filter, pageRequest));
+        assertTrue(exception instanceof  BusinessException);
+        assertEquals("O Id do cliente é obrigatorio", exception.getMessage());
     }
 
-    @Test(expected = EntityNotFoundException.class)
+    @Test()
     public void givenInvalidClientId_whenAllClientInfo_fails() {
         when(clientService.listarPorCodigo(any())).thenThrow(EntityNotFoundException.class);
         InformacaoFilter filter = new InformacaoFilter();
-        Page<ClienteInformacao> result = sut.filtrarAtendimento(2l, filter, pageRequest);
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> sut.filtrarAtendimento(2l, filter, pageRequest));
+        assertTrue(exception instanceof  EntityNotFoundException);
+        assertEquals("O Cliente não encontrado ou não existe", exception.getMessage());
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void giveInvalidClientId_whenRemoveInfoClient_thenFails() {
         doThrow(Exception.class).when(informacaoDao).deleteById(any());
         sut.deleteInfoById(1L);
+        Exception exception = assertThrows(Exception.class, () -> sut.deleteInfoById(1L));
+        assertTrue(exception instanceof  Exception);
     }
 
     @Test
@@ -99,12 +103,14 @@ public class ClientInformationServiceUnitTest {
         sut.deleteInfoById(1L);
     }
 
-    @Test(expected = EntityNotFoundException.class)
+    @Test
     public void giveInvalidClientId_whenSaveInfoClient_thenFails() {
         ClienteInformacao info =  FactoryModels.getClientInfo();
         info.setId(null);
         when(clientService.listarPorCodigo(any())).thenThrow(EntityNotFoundException.class);
-        sut.saveInfo(1L,info);
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> sut.saveInfo(1L,info));
+        assertThat(exception).isInstanceOf(EntityNotFoundException.class);
+        assertEquals("O Cliente não encontrado ou não existe", exception.getMessage());
     }
 
     @Test()
@@ -112,35 +118,41 @@ public class ClientInformationServiceUnitTest {
         ClienteInformacao info =  FactoryModels.getClientInfo();
         info.setId(null);
         ClienteInformacao infoSave = sut.saveInfo(1L,info);
-        Assert.assertNotNull(infoSave);
-        assertThat(infoSave.getCliente().getId(), is(1L));
-        Assert.assertNotNull(infoSave.getId());
+        assertNotNull(infoSave);
+        assertEquals(infoSave.getCliente().getId(), 1L);
+        assertNotNull(infoSave.getId());
     }
 
-    @Test(expected = EntityNotFoundException.class)
+    @Test
     public void giveInvalidClientInfoId_whenUpdateInfoCliente_thenFails() {
+        when(informacaoDao.findById(any())).thenReturn(Optional.empty());
         ClienteInformacao info = FactoryModels.getClientInfo();
         info.setDescricao("Outra Descricao");
-        when(informacaoDao.findById(any())).thenReturn(Optional.empty());
-        sut.atualizarInformacao(1L, 1L,info );
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> sut.atualizarInformacao(1L, 1L,info ));
+        assertTrue(exception instanceof  EntityNotFoundException);
+        assertEquals("Informação não foi encontrada", exception.getMessage());
     }
 
-    @Test(expected = EntityNotFoundException.class)
+    @Test
     public void giveInvalidClientId_whenUpdateInfoCliente_thenFails() {
         ClienteInformacao info = FactoryModels.getClientInfo();
-        info.setDescricao("Outra Descricao");
         when(clientService.listarPorCodigo(any())).thenThrow(EntityNotFoundException.class);
-        sut.atualizarInformacao(1L, 3L,info );
+        info.setDescricao("Outra Descricao");
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> sut.atualizarInformacao(1L, 3L,info ));
+        assertTrue(exception instanceof EntityNotFoundException);
+        assertEquals("O Cliente não encontrado ou não existe", exception.getMessage());
     }
 
-    @Test(expected = BusinessException.class)
+    @Test
     public void giveValidClientInfoIdAndDifferentClientId_whenUpdateInfoCliente_thenFails() {
         Cliente wrongClient = FactoryModels.getCliente();
+        when(clientService.listarPorCodigo(2L)).thenReturn(wrongClient);
         wrongClient.setId(2L);
         ClienteInformacao info = FactoryModels.getClientInfo();
         info.setDescricao("Outra Descricao");
-        when(clientService.listarPorCodigo(2L)).thenReturn(wrongClient);
-        sut.atualizarInformacao(1L, 2L,info );
+        Exception exception = assertThrows(BusinessException.class, () ->  sut.atualizarInformacao(1L, 2L,info ));
+        assertTrue(exception instanceof BusinessException);
+        assertEquals("Erro ao realizar atualização!", exception.getMessage());
     }
 
     @Test
@@ -149,9 +161,9 @@ public class ClientInformationServiceUnitTest {
         info.setDescricao("Outra Descricao");
         when(informacaoDao.save(any())).thenReturn(info);
         ClienteInformacao updateInfo = sut.atualizarInformacao(1L, 1L,info );
-        assertThat(updateInfo.getId(), is(info.getId()));
-        assertThat(updateInfo.getDescricao(), is("Outra Descricao"));
-        assertThat(updateInfo.getCliente().getId(), is(info.getCliente().getId()));
+        assertEquals(updateInfo.getId(), info.getId());
+        assertEquals(updateInfo.getDescricao(), "Outra Descricao");
+        assertEquals(updateInfo.getCliente().getId(), info.getCliente().getId());
     }
 
 }
