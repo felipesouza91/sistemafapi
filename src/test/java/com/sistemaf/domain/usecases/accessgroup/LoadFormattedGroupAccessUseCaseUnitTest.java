9e1ecbce83ae30ec29d2f913494cabd071b4e2e4
Felipe Souza <felipedb91@gmail.com>
2023-04-03 20:14:22 -0300

    refactor: update LoadFormattedGroupAccessUseCaseUnitTest

--- src/test/java/com/sistemaf/domain/usecases/accessgroup/LoadFormattedGroupAccessUseCaseUnitTest.java
+++ src/test/java/com/sistemaf/domain/usecases/accessgroup/LoadFormattedGroupAccessUseCaseUnitTest.java
@@ -4,15 +4,16 @@ import com.sistemaf.domain.exception.EntityNotFoundException;
 import com.sistemaf.domain.repository.security.grupoacesso.GrupoAcessoRepository;
 import com.sistemaf.domain.usecases.permission.PermissionMapperUseCase;
 import com.sistemaf.util.FactoryModels;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 import static org.testcontainers.shaded.org.hamcrest.MatcherAssert.assertThat;
@@ -20,7 +21,7 @@ import static org.testcontainers.shaded.org.hamcrest.Matchers.is;
 import static org.testcontainers.shaded.org.hamcrest.Matchers.not;
 
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class LoadFormattedGroupAccessUseCaseUnitTest {
 
   @Mock
@@ -32,24 +33,22 @@ public class LoadFormattedGroupAccessUseCaseUnitTest {
   @InjectMocks
   private LoadFormattedGroupAccessUseCase sut;
 
-  @Before
-  public void setup() {
-    when(permissionMapperUseCase.toListDto(any())).thenReturn(FactoryModels.getFormatedPermissinByGroup());
-    when(repository.findById(any())).thenReturn(Optional.of(FactoryModels.getGrupoAcesso()));
-  }
 
   @Test
   public void giveValidGroupId_whenFindById_success() {
+    when(permissionMapperUseCase.toListDto(any())).thenReturn(FactoryModels.getFormatedPermissinByGroup());
+    when(repository.findById(any())).thenReturn(Optional.of(FactoryModels.getGrupoAcesso()));
     var permissionGroupDto =  sut.execute(1L);
     assertThat(permissionGroupDto.getId(), is(1L) );
     assertThat(permissionGroupDto.getPermissions().size(), not(is(0)));
     assertThat(permissionGroupDto.getPermissions().stream().anyMatch(item -> item.getNameId().equals("CLIENTE") && !item.getWrite()), is(true));
   }
 
-  @Test(expected = EntityNotFoundException.class)
+  @Test()
   public void giveInvalidGroupIp_whenExecute_thenErro() {
     when(repository.findById(any())).thenReturn(Optional.empty());
-    sut.execute(2L);
+    Exception exception = assertThrows(EntityNotFoundException.class, () -> sut.execute(2L));
+    assertTrue(exception instanceof  EntityNotFoundException);
   }
 
 }
