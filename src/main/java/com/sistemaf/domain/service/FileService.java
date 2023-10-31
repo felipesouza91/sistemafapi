0b4ce86f9c3c357cf2dc99c5c7e69e3d7df7943f
Felipe Souza <felipedb91@gmail.com>
2023-10-30 22:20:49 -0300

    feat: create FileService definition

---
+++ src/main/java/com/sistemaf/domain/service/FileService.java
@@ -0,0 +1,9 @@
+package com.sistemaf.domain.service;
+
+import com.sistemaf.domain.model.definition.FileReference;
+
+import java.net.URL;
+
+public interface FileService {
+    URL generateUploadSignedUrl(FileReference fileReference);
+}
