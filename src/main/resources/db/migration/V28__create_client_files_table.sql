42f6901252102ecf50b84d1d5e035ca6046c4b2a
Felipe Souza <felipedb91@gmail.com>
2023-11-16 22:07:16 -0300

    fix: update columns names

--- src/main/resources/db/migration/V28__create_client_files_table.sql
+++ src/main/resources/db/migration/V28__create_client_files_table.sql
@@ -1,9 +1,10 @@
 CREATE TABLE client_files (
     id char(36) primary key not null,
-    fileName varchar(255) not null,
-    originalFileName varchar(255),
-    contentType varchar(20),
+    file_name varchar(255) not null,
+    original_file_name varchar(255),
+    content_type varchar(20),
     temp boolean not null,
     client_id bigint(20) not null,
+    created_at datetime not null,
     FOREIGN KEY (client_id) REFERENCES cliente(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
