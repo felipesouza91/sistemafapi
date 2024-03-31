493526c0f460e4d212f8a0c01fe93f7d4bc67432
Felipe Souza <felipedb91@gmail.com>
2024-03-31 16:42:53 -0300

    fix: update item stock sql script

--- src/main/resources/db/migration/V29_create_item_stock.sql
+++ src/main/resources/db/migration/V29_create_item_stock.sql
@@ -7,7 +7,12 @@ CREATE TABLE if NOT EXISTS items_stock (
     id VARCHAR(32) DEFAULT (uuid()) PRIMARY KEY NOT NULL,
     serial varchar(255) NOT NULL UNIQUE,
     product_id BIGINT(20) NOT NULL,
-    status BOOLEAN NOT NULL,
+    active BOOLEAN NOT NULL,
     created_at datetime NOT NULL,
-    CONSTRAINT fk_product_id_item_stock FOREIGN KEY (product_id) REFERENCES produto(id)
+    updated_at datetime NOT NULL,
+    created_by BIGINT(20) NOT NULL,
+    updated_by BIGINT(20),
+    CONSTRAINT fk_product_id_item_stock FOREIGN KEY (product_id) REFERENCES produto(id),
+    CONSTRAINT fk_create_by_items_stock FOREIGN KEY (created_by) REFERENCES usuario(id),
+    CONSTRAINT fk_updated_by_items_stock FOREIGN KEY (created_by) REFERENCES usuario(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
