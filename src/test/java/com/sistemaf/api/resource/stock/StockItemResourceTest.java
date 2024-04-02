8604dd2f03d95f65730302cec168e419616ae236
Felipe Souza <felipedb91@gmail.com>
2024-04-01 21:57:22 -0300

    test: ensure return 201 and stock item data

--- src/test/java/com/sistemaf/api/resource/stock/StockItemResourceTest.java
+++ src/test/java/com/sistemaf/api/resource/stock/StockItemResourceTest.java
@@ -2,7 +2,11 @@ package com.sistemaf.api.resource.stock;
 
 import com.sistemaf.api.dto.input.StockItemInput;
 import com.sistemaf.domain.contracts.stock.AddStockItemService;
+import com.sistemaf.domain.exception.BusinessException;
+import com.sistemaf.domain.model.Produto;
+import com.sistemaf.domain.model.StockItem;
 import com.sistemaf.util.BaseWebMvcTestConfig;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -57,5 +62,38 @@ public class StockItemResourceTest extends BaseWebMvcTestConfig {
                         .content(super.objectMapper.writeValueAsString(input))
                         .with(csrf()));
         verify(addStockItemService, times(1)).perform(any());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("should return 400 if product id not exists")
+    public void givenInvalidProductId_whenSaveItem_thenReturnBadRequest() throws Exception {
+        StockItemInput input = new StockItemInput("ASDADAD", 1L);
+        given(addStockItemService.perform(any())).willThrow(new BusinessException("O produto não foi encontrado ou esta desabilitado"));
+        mockMvc.perform(post(STOCK_ITEM_PATH_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(super.objectMapper.writeValueAsString(input))
+                .with(csrf()))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.title", is("Violação de regra de negocio")))
+                .andExpect(jsonPath("$.detail", is("O produto não foi encontrado ou esta desabilitado")));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("should return 201 with stock item data")
+    public void givenValidInput_whenSaveItem_thenReturnCreated() throws Exception {
+        Produto produto = Instancio.create(Produto.class);
+        StockItem stockItem = Instancio.create(StockItem.class);
+        stockItem.setProduto(produto);
+        StockItemInput input = new StockItemInput("ASDADAD", produto.getId());
+        given(addStockItemService.perform(any())).willReturn(stockItem);
+        mockMvc.perform(post(STOCK_ITEM_PATH_REQUEST)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(super.objectMapper.writeValueAsString(input))
+                        .with(csrf()))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(stockItem.getId().toString())))
+                .andExpect(jsonPath("$.product.id", is(produto.getId().intValue())));
     }
 }
