0f1e98dde9068bcc3f08e902a00a81d0902f36be
Felipe Souza <felipedb91@gmail.com>
2024-04-09 20:39:23 -0300

    feat: ensure call findBySerial with correct value

---
+++ src/main/java/com/sistemaf/domain/repository/StockItemRepository.java
@@ -0,0 +1,9 @@
+package com.sistemaf.domain.repository;
+
+import com.sistemaf.domain.model.StockItem;
+
+import java.util.Optional;
+
+public interface StockItemRepository {
+    Optional<StockItem> findBySerial(String serial);
+}
--- src/main/java/com/sistemaf/domain/usecases/stock/AddStockItemUseCase.java
+++ src/main/java/com/sistemaf/domain/usecases/stock/AddStockItemUseCase.java
@@ -2,13 +2,19 @@ package com.sistemaf.domain.usecases.stock;
 
 import com.sistemaf.domain.contracts.stock.AddStockItemService;
 import com.sistemaf.domain.model.StockItem;
+import com.sistemaf.domain.repository.StockItemRepository;
 import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AddStockItemUseCase implements AddStockItemService {
+
+    private final StockItemRepository stockItemRepository;
     @Override
     public StockItem perform(StockItem data) {
+        this.stockItemRepository.findBySerial(data.getSerial());
         return null;
     }
 }
