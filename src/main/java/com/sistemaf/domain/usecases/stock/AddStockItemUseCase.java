f8a6e26b083b7e2110c2094147c095bf9dabea5b
Felipe Souza <felipedb91@gmail.com>
2024-04-09 21:03:35 -0300

    feat: ensure throws if product not exits

--- src/main/java/com/sistemaf/domain/usecases/stock/AddStockItemUseCase.java
+++ src/main/java/com/sistemaf/domain/usecases/stock/AddStockItemUseCase.java
@@ -2,9 +2,10 @@ package com.sistemaf.domain.usecases.stock;
 
 import com.sistemaf.domain.contracts.stock.AddStockItemService;
 import com.sistemaf.domain.exception.BusinessException;
+import com.sistemaf.domain.model.Produto;
 import com.sistemaf.domain.model.StockItem;
 import com.sistemaf.domain.repository.StockItemRepository;
-import io.swagger.v3.oas.annotations.servers.Server;
+import com.sistemaf.domain.repository.estoque.produto.ProdutoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +15,18 @@ import java.util.Optional;
 @AllArgsConstructor
 public class AddStockItemUseCase implements AddStockItemService {
 
+    private final ProdutoRepository productRepository;
+
     private final StockItemRepository stockItemRepository;
     @Override
     public StockItem perform(StockItem data) {
         Optional<StockItem> usedStockItem = this.stockItemRepository.findBySerial(data.getSerial());
         if(usedStockItem.isPresent()) {
             throw new BusinessException("O serial já esta cadastrado");
+        }
+        Optional<Produto> productExists = this.productRepository.findById(data.getProduto().getId());
+        if( productExists.isEmpty()) {
+            throw new BusinessException("O produto informado não foi encontrado");
         }
         return this.stockItemRepository.save(data);
     }
