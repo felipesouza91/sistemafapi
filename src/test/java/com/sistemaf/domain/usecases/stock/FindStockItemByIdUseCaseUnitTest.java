f5e900544b0e68f35506feb008984cce1a96eba7
Felipe Souza <felipedb91@gmail.com>
2024-04-09 22:32:59 -0300

    test: ensure return StockItem if id exits

--- src/test/java/com/sistemaf/domain/usecases/stock/FindStockItemByIdUseCaseUnitTest.java
+++ src/test/java/com/sistemaf/domain/usecases/stock/FindStockItemByIdUseCaseUnitTest.java
@@ -1,7 +1,9 @@
 package com.sistemaf.domain.usecases.stock;
 
 import com.sistemaf.domain.exception.EntityNotFoundException;
+import com.sistemaf.domain.model.StockItem;
 import com.sistemaf.domain.repository.StockItemRepository;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,13 +30,15 @@ class FindStockItemByIdUseCaseUnitTest {
     @InjectMocks
     private FindStockItemByIdUseCase findStockItemByIdUseCase;
 
+    private StockItem data = Instancio.create(StockItem.class);
 
     @Test
     @DisplayName("should call repository with correct data ")
     public void givenID_thenPerformUseCase_thenCallRepositoryWithCorrectData() {
-        UUID id = UUID.randomUUID();
-        findStockItemByIdUseCase.perform(id);
-        verify(stockItemRepository, times(1)).findById(id);
+
+        given(stockItemRepository.findById(data.getId())).willReturn(Optional.of(data));
+        findStockItemByIdUseCase.perform(data.getId());
+        verify(stockItemRepository, times(1)).findById(data.getId());
     }
 
     @Test
@@ -44,5 +48,15 @@ class FindStockItemByIdUseCaseUnitTest {
         given(stockItemRepository.findById(id)).willReturn(Optional.empty());
         Exception exception = assertThrows(EntityNotFoundException.class, () -> findStockItemByIdUseCase.perform(id));
         assertThat(exception.getMessage(), is("O Item não foi encontrado"));
+    }
+
+    @Test
+    @DisplayName("should return Stock Item if id exits")
+    public void givenValidID_thenPerformUseCase_thenReturnStockItem() {
+        given(stockItemRepository.findById(data.getId())).willReturn(Optional.of(data));
+        StockItem stockItem = findStockItemByIdUseCase.perform(data.getId());
+        assertThat(data.getSerial(), is(stockItem.getSerial()));
+        assertThat(data.getId(), is(stockItem.getId()));
+
     }
 }
