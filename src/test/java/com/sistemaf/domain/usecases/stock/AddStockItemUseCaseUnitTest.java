20df3f8be604859f08c0d1faa2f341d2d6d81d35
Felipe Souza <felipedb91@gmail.com>
2024-04-09 21:18:03 -0300

    refactor: refactor test

--- src/test/java/com/sistemaf/domain/usecases/stock/AddStockItemUseCaseUnitTest.java
+++ src/test/java/com/sistemaf/domain/usecases/stock/AddStockItemUseCaseUnitTest.java
@@ -1,7 +1,6 @@
 package com.sistemaf.domain.usecases.stock;
 
 
-import com.sistemaf.api.dto.model.resume.ProductResumeModel;
 import com.sistemaf.domain.exception.BusinessException;
 import com.sistemaf.domain.model.StockItem;
 import com.sistemaf.domain.repository.StockItemRepository;
@@ -19,7 +18,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.BDDMockito.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -38,65 +36,66 @@ class AddStockItemUseCaseUnitTest {
     @Spy
     @InjectMocks
     private AddStockItemUseCase addStockItemUseCase;
+    StockItem inputData = Instancio.create(StockItem.class);
 
     @Test
     @DisplayName("should called with correct values")
     public void givenInputData_whenExecuteUseCase_thenCalledWithCorrectItem() {
-        StockItem stockItem = Instancio.create(StockItem.class);
-        stockItem.setId(null);
-        addStockItemUseCase.perform(stockItem);
-        verify(addStockItemUseCase, times(1)).perform(stockItem);
+        inputData.setId(null);
+        given(productRepository.findById(inputData.getProduto().getId())).willReturn(Optional.of(inputData.getProduto()));
+        addStockItemUseCase.perform(inputData);
+        verify(addStockItemUseCase, times(1)).perform(inputData);
     }
 
     @Test
     @DisplayName("should call findBySerial repository with correct value")
     public void givenInputData_whenExecuteUseCase_thenCalledFindBySerialRepository() {
-        StockItem stockItem = Instancio.create(StockItem.class);
-        stockItem.setId(null);
-        addStockItemUseCase.perform(stockItem);
-        verify(stockItemRepository, times(1)).findBySerial(stockItem.getSerial());
+        inputData.setId(null);
+        given(productRepository.findById(inputData.getProduto().getId())).willReturn(Optional.of(inputData.getProduto()));
+
+        addStockItemUseCase.perform(inputData);
+        verify(stockItemRepository, times(1)).findBySerial(inputData.getSerial());
     }
 
     @Test
     @DisplayName("should call throw if serial exists")
     public void givenInputData_whenExecuteUseCase_thenThrowsIfSerialExists() {
-        StockItem stockItem = Instancio.create(StockItem.class);
-        stockItem.setId(null);
-        given(stockItemRepository.findBySerial(stockItem.getSerial())).willReturn(Optional.of(stockItem));
-        Exception exception = assertThrows(BusinessException.class, () ->  addStockItemUseCase.perform(stockItem));
+        inputData.setId(null);
+
+        given(stockItemRepository.findBySerial(inputData.getSerial())).willReturn(Optional.of(inputData));
+        Exception exception = assertThrows(BusinessException.class, () ->  addStockItemUseCase.perform(inputData));
         assertThat(exception.getMessage(), is("O serial já esta cadastrado"));
     }
 
     @Test
     @DisplayName("should call save stock item repository method with correct values")
     public void givenInputData_whenExecuteUseCase_thenCallSaveRepositoryWithCorrectData() {
-        StockItem stockItem = Instancio.create(StockItem.class);
-        stockItem.setId(null);
-        addStockItemUseCase.perform(stockItem);
-        verify(stockItemRepository, times(1)).save(stockItem);
+        inputData.setId(null);
+        given(productRepository.findById(inputData.getProduto().getId())).willReturn(Optional.of(inputData.getProduto()));
+        addStockItemUseCase.perform(inputData);
+        verify(stockItemRepository, times(1)).save(inputData);
     }
 
     @Test
     @DisplayName("should return saved stock item")
     public void givenInputData_whenExecuteUseCase_thenReturnSavedStockItem() {
-        StockItem stockItem = Instancio.create(StockItem.class);
-        stockItem.setId(null);
-        given(stockItemRepository.findBySerial(stockItem.getSerial())).willReturn(Optional.empty());
-        given(stockItemRepository.save(stockItem)).will( args -> {
+        inputData.setId(null);
+        given(productRepository.findById(inputData.getProduto().getId())).willReturn(Optional.of(inputData.getProduto()));
+        given(stockItemRepository.findBySerial(inputData.getSerial())).willReturn(Optional.empty());
+        given(stockItemRepository.save(inputData)).will( args -> {
             StockItem stockItem1 = args.getArgument(0);
             stockItem1.setId(UUID.randomUUID());
             return stockItem1;
         });
-        StockItem savedStockItem =  addStockItemUseCase.perform(stockItem);
+        StockItem savedStockItem =  addStockItemUseCase.perform(inputData);
         assertNotNull(savedStockItem.getId());
-        assertThat(savedStockItem.getSerial(), is(stockItem.getSerial()));
+        assertThat(savedStockItem.getSerial(), is(inputData.getSerial()));
         assertNotNull(savedStockItem.getCreatedAt());
     }
 
     @Test
     @DisplayName("should throws if product do not exits")
     public void givenInvalidProduct_whenExecuteUseCase_thenThrows() {
-        StockItem inputData = Instancio.create(StockItem.class);
         inputData.setId(null);
         given(productRepository.findById(inputData.getProduto().getId())).willReturn(Optional.empty());
         Exception exception = assertThrows(BusinessException.class, () -> addStockItemUseCase.perform(inputData));
