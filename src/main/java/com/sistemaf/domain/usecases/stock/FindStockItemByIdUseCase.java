d62333fa189b02b01b90135836cab73f6c7f5e4d
Felipe Souza <felipedb91@gmail.com>
2024-04-09 22:28:28 -0300

    feat: ensure throws if stock item not found

--- src/main/java/com/sistemaf/domain/usecases/stock/FindStockItemByIdUseCase.java
+++ src/main/java/com/sistemaf/domain/usecases/stock/FindStockItemByIdUseCase.java
@@ -1,11 +1,13 @@
 package com.sistemaf.domain.usecases.stock;
 
 import com.sistemaf.domain.contracts.stock.FindStockItemByIdService;
+import com.sistemaf.domain.exception.EntityNotFoundException;
 import com.sistemaf.domain.model.StockItem;
 import com.sistemaf.domain.repository.StockItemRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -15,7 +17,10 @@ public class FindStockItemByIdUseCase implements FindStockItemByIdService {
     private final StockItemRepository stockItemRepository;
     @Override
     public StockItem perform(UUID id) {
-        this.stockItemRepository.findById(id);
+        Optional<StockItem> existStockItem =  this.stockItemRepository.findById(id);
+        if(existStockItem.isEmpty()) {
+            throw new EntityNotFoundException("O Item não foi encontrado");
+        }
         return null;
     }
 }
