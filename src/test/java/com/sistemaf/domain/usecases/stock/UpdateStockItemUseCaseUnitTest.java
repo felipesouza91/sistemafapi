86bc863c4d82480cb6316195451487744f5b3510
Felipe Souza <felipedb91@gmail.com>
2024-04-15 20:54:55 -0300

    test: ensure throws when id not found

--- src/test/java/com/sistemaf/domain/usecases/stock/UpdateStockItemUseCaseUnitTest.java
+++ src/test/java/com/sistemaf/domain/usecases/stock/UpdateStockItemUseCaseUnitTest.java
@@ -1,33 +1,56 @@
 package com.sistemaf.domain.usecases.stock;
 
+import com.sistemaf.domain.exception.EntityNotFoundException;
 import com.sistemaf.domain.model.StockItem;
+import com.sistemaf.domain.repository.StockItemRepository;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+import java.util.Random;
 import java.util.UUID;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class UpdateStockItemUseCaseUnitTest {
 
+    @Mock
+    private StockItemRepository stockItemRepository;
 
     @Spy
+    @InjectMocks
     private UpdateStockItemUseCase updateStockItemUseCase;
 
+    private final StockItem mockInputValue = Instancio.of(StockItem.class).ignore(field(StockItem::getId)).create();
 
     @Test
     @DisplayName("should call service with correct data")
     public void given_whenUpdateStockItem_thenCallWithCorrectItem() {
-        StockItem stockItem = Instancio.create(StockItem.class);
-        stockItem.setId(null);
+
         UUID id = UUID.randomUUID();
-        updateStockItemUseCase.perform(id, stockItem);
-        verify(updateStockItemUseCase, times(1)).perform(id, stockItem);
+        updateStockItemUseCase.perform(id, mockInputValue);
+        verify(updateStockItemUseCase, times(1)).perform(id, mockInputValue);
+    }
+
+    @Test
+    @DisplayName("should throws if id not exits")
+    public void givenInvalidId_whenUpdateStockItem_thenThrows() {
+        UUID id = UUID.randomUUID();
+        given(stockItemRepository.findById(id)).willReturn(Optional.empty());
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> updateStockItemUseCase.perform(id, mockInputValue));
+        assertThat(exception.getMessage(), is("O item não foi encontrado"));
     }
 }
