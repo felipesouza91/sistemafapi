10900e1ee58373fb85399e57749f18ca4856d228
Felipe Souza <felipedb91@gmail.com>
2024-04-15 20:37:25 -0300

    test: ensure return a not null object

--- src/test/java/com/sistemaf/domain/usecases/stock/FindStockItemServicesUseCaseUnitTest.java
+++ src/test/java/com/sistemaf/domain/usecases/stock/FindStockItemServicesUseCaseUnitTest.java
@@ -1,6 +1,7 @@
 package com.sistemaf.domain.usecases.stock;
 
 import com.sistemaf.domain.filter.StockItemFilter;
+import com.sistemaf.domain.model.StockItem;
 import com.sistemaf.domain.repository.StockItemRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,8 +9,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
+import java.util.Collections;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNotNull;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -28,5 +37,14 @@ class FindStockItemServicesUseCaseUnitTest {
         StockItemFilter stockItemFilter = new StockItemFilter();
         findStockItemServicesUseCase.perform(stockItemFilter, null);
         verify(repository, times(1)).findWithFilter(stockItemFilter, null);
+    }
+
+    @Test
+    @DisplayName("should return a value")
+    public void given_whenFind_thenReturnValues() {
+        StockItemFilter stockItemFilter = new StockItemFilter();
+        given(repository.findWithFilter(any(),any())).willReturn(new PageImpl<>(Collections.emptyList()));
+        Page<StockItem> stockItems = findStockItemServicesUseCase.perform(stockItemFilter, null);
+        assertNotNull(stockItems.getContent());
     }
 }
