de9c9f2d5234a01f42b474e8e02dee44ee649d8f
Felipe Souza <felipedb91@gmail.com>
2024-04-15 21:00:25 -0300

    feat: ensure throws when product id not exits

--- src/main/java/com/sistemaf/domain/usecases/stock/UpdateStockItemUseCase.java
+++ src/main/java/com/sistemaf/domain/usecases/stock/UpdateStockItemUseCase.java
@@ -1,9 +1,12 @@
 package com.sistemaf.domain.usecases.stock;
 
 import com.sistemaf.domain.contracts.stock.UpdateStockItemService;
+import com.sistemaf.domain.exception.BusinessException;
 import com.sistemaf.domain.exception.EntityNotFoundException;
+import com.sistemaf.domain.model.Produto;
 import com.sistemaf.domain.model.StockItem;
 import com.sistemaf.domain.repository.StockItemRepository;
+import com.sistemaf.domain.repository.estoque.produto.ProdutoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +18,17 @@ import java.util.UUID;
 public class UpdateStockItemUseCase implements UpdateStockItemService {
 
     private StockItemRepository stockItemRepository;
+    private ProdutoRepository productRepositoy;
 
     @Override
     public StockItem perform(UUID id, StockItem data) {
         Optional<StockItem> exitsStockItem = this.stockItemRepository.findById(id);
         if(exitsStockItem.isEmpty()) {
             throw new EntityNotFoundException("O item não foi encontrado");
+        }
+        Optional<Produto> exitsDataProduct = this.productRepositoy.findById(data.getProduto().getId());
+        if(exitsDataProduct.isEmpty()) {
+            throw new EntityNotFoundException("O produto não foi encontrado");
         }
         return null;
     }
