214704a71a11609475e304e7b1f83f30182ebc71
Felipe Souza <felipedb91@gmail.com>
2024-04-17 22:11:16 -0300

    feat: add script to add stock items permissions in administrator access group.

---
+++ src/main/resources/db/migration/V30__add_permition_to_administrator_group.sql
@@ -0,0 +1,3 @@
+INSERT INTO grupo_permissao (codigo_grupo, codigo_permissao) value(1,55);
+INSERT INTO grupo_permissao (codigo_grupo, codigo_permissao) value(1,56);
+INSERT INTO grupo_permissao (codigo_grupo, codigo_permissao) value(1,57);
