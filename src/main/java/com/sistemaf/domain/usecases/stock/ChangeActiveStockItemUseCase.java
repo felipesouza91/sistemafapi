b3c762baa7f3ee9074eb9f1978c208ff94e754b5
Felipe Souza <felipedb91@gmail.com>
2024-04-30 21:10:42 -0300

    feat: ensure throws if id not exits

--- src/main/java/com/sistemaf/domain/usecases/stock/ChangeActiveStockItemUseCase.java
+++ src/main/java/com/sistemaf/domain/usecases/stock/ChangeActiveStockItemUseCase.java
@@ -1,10 +1,13 @@
 package com.sistemaf.domain.usecases.stock;
 
 import com.sistemaf.domain.contracts.stock.ChangeActiveStockItemService;
+import com.sistemaf.domain.exception.EntityNotFoundException;
+import com.sistemaf.domain.model.StockItem;
 import com.sistemaf.domain.repository.estoque.stockitem.StockItemRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -14,6 +17,9 @@ public class ChangeActiveStockItemUseCase implements ChangeActiveStockItemServic
     private StockItemRepository stockItemRepository;
     @Override
     public void perform(UUID id, Boolean active) {
-        this.stockItemRepository.findById(id);
+        Optional<StockItem> findStockItem = this.stockItemRepository.findById(id);
+        if(findStockItem.isEmpty()) {
+            throw new EntityNotFoundException("O item do stock nao foi encontrado");
+        }
     }
 }
