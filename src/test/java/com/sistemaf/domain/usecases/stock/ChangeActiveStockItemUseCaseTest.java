a7425819c11402b7f55a10d5db7f6f3fcdb58ad7
Felipe Souza <felipedb91@gmail.com>
2024-04-30 21:10:46 -0300

    test: ensure throws if id not exits

--- src/test/java/com/sistemaf/domain/usecases/stock/ChangeActiveStockItemUseCaseTest.java
+++ src/test/java/com/sistemaf/domain/usecases/stock/ChangeActiveStockItemUseCaseTest.java
@@ -1,5 +1,6 @@
 package com.sistemaf.domain.usecases.stock;
 
+import com.sistemaf.domain.exception.EntityNotFoundException;
 import com.sistemaf.domain.repository.estoque.stockitem.StockItemRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -28,5 +32,13 @@ class ChangeActiveStockItemUseCaseTest {
         UUID id = UUID.randomUUID();
         sut.perform(id, true);
         verify(stockItemRepository, times(1)).findById(id);
+    }
+
+    @Test
+    @DisplayName("should throws if id not find")
+    public void givenInvalidId_whenPerform_thenThrows() {
+        UUID id = UUID.randomUUID();
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> sut.perform(id, true));
+        assertThat(exception.getMessage(), is("O item do stock nao foi encontrado"));
     }
 }
