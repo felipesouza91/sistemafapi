4c953c20bb78801729d03b382ca089f64b527a18
Felipe Souza <felipedb91@gmail.com>
2024-05-03 22:26:19 -0300

    fix: change return type form field active

--- src/main/java/com/sistemaf/api/dto/model/StockitemResumeDTO.java
+++ src/main/java/com/sistemaf/api/dto/model/StockitemResumeDTO.java
@@ -1,5 +1,6 @@
 package com.sistemaf.api.dto.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,16 +8,18 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Schema(name = "Stock Item Model Resume")
 public class StockitemResumeDTO {
 
+    @Schema(description =  "Stock Item Id", example = "1")
     private UUID id;
-
+    @Schema(description =  "Serial ", example = "SD0000")
     private String serial;
-
-    private String active;
-
+    @Schema(description =  "Active status", example = "true")
+    private Boolean active;
+    @Schema(description =  "Product name", example = "Product name")
     private String productModel;
-
+    @Schema(description =  "Manufacture name", example = "Manufacture name")
     private String manufactureName;
 
 
